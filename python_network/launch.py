c47f0d75aa6c2c90d59ecbd778e6141d38c0c102
Mateusz-of-Rivia <mtsmolar@student.agh.edu.pl>
2025-04-09 22:51:25 +0200

    refactor processing

--- python_network/launch.py
+++ python_network/launch.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import cv2
 import torch
+from scipy.spatial.transform import Rotation as R
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -104,11 +105,32 @@ def compute_sequence(image_folder, model, tryb='show'):
     all_times = []
     matches_list = []
     matches = []
-    for i in range(0, len(image_files), 3):
+    R_list = []
+    fx = 517.3  # focal length x
+    fy = 516.5  # focal length y
+    cx = 318.6  # optical center x
+    cy = 255.3 # optical center y
+    K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+
+    # Translacja
+    t_total = np.array([[1.3563],
+                        [0.6305],
+                        [1.6380]])
+
+    # Rotacja z kwaternionu
+    q = [0.6132, 0.5962, -0.3311, -0.3986] 
+    R_total = R.from_quat(q).as_matrix()
+    trajectory = [t_total.flatten().tolist()]
+    R_list = [R_total.tolist()]
+    lenght = len(image_files)
+    for i in range(0, 20):
+        print(f"Processing: {i/20 * 100}%")
         image_file = image_files[i]
         image_path = os.path.join(image_folder, image_file)
         time1 = time.perf_counter()
-        image, img_orig = processing.preprocess_image(image_path, (200, 200))
+        image, img_orig = processing.preprocess_image(image_path, (width, height))
+
+
         time2 = time.perf_counter()
         scores, descriptors_dense = model(torch.from_numpy(image[None, None]).float())
         time3 = time.perf_counter()
@@ -123,10 +145,19 @@ def compute_sequence(image_folder, model, tryb='show'):
             H, inliers = processing.compute_homography(m_kp1, m_kp2)
             matches = np.array(matches)[inliers.astype(bool)].tolist()
 
-            for match in matches:
-                pt1 = tuple(map(int, past_keypoints[match.queryIdx].pt))
-                pt2 = tuple(map(int, keypoints[match.trainIdx].pt))
-                cv2.arrowedLine(img_orig, pt2, pt1, (0, 255, 0), 1, tipLength=0.2)
+            if tryb == 'show':
+                for match in matches:
+                    pt1 = tuple(map(int, past_keypoints[match.queryIdx].pt))
+                    pt2 = tuple(map(int, keypoints[match.trainIdx].pt))
+                    cv2.arrowedLine(img_orig, pt2, pt1, (0, 255, 0), 1, tipLength=0.2)
+
+            if tryb == "points":
+                points = np.array([[past_keypoints[match.queryIdx].pt, keypoints[match.trainIdx].pt] for match in matches], dtype=np.float32)
+                m_kp1_float = points[:, 0]
+                m_kp2_float = points[:, 1]
+                t_total, R_total = pose_estimation(m_kp1_float, m_kp2_float, K, t_total, R_total)
+                trajectory.append(t_total.flatten().tolist())
+                R_list.append(R_total.tolist())
 
         past_descriptors = descriptors
         past_keypoints = keypoints
@@ -147,6 +178,7 @@ def compute_sequence(image_folder, model, tryb='show'):
             matching_times.append(end - time4)
             all_times.append(end-time1)
             matches_list.append(len(matches))
+
     if tryb=='show':
         cv2.destroyAllWindows()
     if tryb=='time':
@@ -157,11 +189,69 @@ def compute_sequence(image_folder, model, tryb='show'):
         avg_matching = sum(matching_times[1:]) / num_iterations * 1000
         avg_all = sum(all_times[1:]) / num_iterations * 1000
         avg_matches = sum(matches_list[1:]) / num_iterations
+        return avg_pre, avg_net, avg_post, avg_matching, avg_all, 
+    if tryb == 'points':
+        return np.array(trajectory), np.array(R_list)
+        
+def pose_estimation(points1, points2, K, t_total, R_total):
+    E, mask = cv2.findEssentialMat(points1, points2, K,  method=cv2.RANSAC, prob=0.999, threshold=3.0)
+    points, R, t, mask = cv2.recoverPose(E, points1, points2, K)
+    R_total = R_total @ R
+    t_total = t_total + R_total @ t
+    return t_total, R_total
 
-        return avg_pre, avg_net, avg_post, avg_matching, avg_all, avg_matches
 
 model = superpoint_pytorch.SuperPoint_short(detection_threshold=detection_thresh, nms_radius=nms_radius).eval()
 model.load_state_dict(torch.load("weights/superpoint_v6_from_tf.pth"))
 image_folder = "data//rgbd_dataset_freiburg1_xyz//rgb"
-avg_pre, avg_net, avg_post, avg_matching, avg_all, avg_matches = compute_sequence(image_folder, model, tryb='time')
-print(f"Średnie czasy (ms): pre: {avg_pre:.6f} | net: {avg_net:.6f} | post: {avg_post:.6f} | matching: {avg_matching:.6f} | all: {avg_all:.6f} | matches: {avg_matches:.6f}")
+trajectory, rotations_est = compute_sequence(image_folder, model, tryb='points')
+# # print(f"Średnie czasy (ms): pre: {avg_pre:.6f} | net: {avg_net:.6f} | post: {avg_post:.6f} | matching: {avg_matching:.6f} | all: {avg_all:.6f} | matches: {avg_matches:.6f}")
+trajectorygt = np.loadtxt(r"data\rgbd_dataset_freiburg1_xyz\groundtruth.txt")[:20]  # lub zamiast pliku: zrób z listy stringów
+
+
+import matplotlib.pyplot as plt
+
+# Wczytanie danych
+
+# Rozdziel translacje
+tx, ty, tz = trajectory[:, 0], trajectory[:, 1], trajectory[:, 2]
+txgt, tygt, tzgt = trajectorygt[:, 1], trajectorygt[:, 2], trajectorygt[:, 3]
+# Wykres 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(tx, ty, tz, label='Trajektoria kamery', color='blue')
+ax.plot(txgt, tygt, tzgt, label='Trajektoria kamery gt', color='red')
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+ax.set_title("Trajektoria kamery 3D")
+ax.legend()
+plt.show()
+
+def rotation_angles_from_matrices(rotation_list):
+    return [np.degrees(R.from_matrix(Ri).magnitude()) for Ri in rotation_list]
+
+# Konwersja kwaternionów do kątów (jeśli masz GT w postaci kwaternionów)
+def rotation_angles_from_quaternions(quat_list):
+    return [np.degrees(R.from_quat(q).magnitude()) for q in quat_list]
+
+# Przykład: jeśli GT jest w kwaternionach
+angles_est = rotation_angles_from_matrices(rotations_est)
+angles_gt = rotation_angles_from_quaternions(trajectorygt[:, 4:])
+
+# Wyrównaj długość (jeśli GT ma więcej klatek)
+min_len = min(len(angles_est), len(angles_gt))
+angles_est = angles_est[:min_len]
+angles_gt = angles_gt[:min_len]
+
+# Wykres
+x = range(min_len)
+
+plt.plot(x, angles_est, label="Odometria estymowana", color="blue")
+plt.plot(x, angles_gt, label="Ground Truth", color="green", linestyle="--")
+plt.xlabel("Klatka")
+plt.ylabel("Kąt obrotu (°)")
+plt.title("Porównanie kąta obrotu kamery")
+plt.legend()
+plt.grid()
+plt.show()
--- python_network/processing.py
+++ python_network/processing.py
@@ -2,90 +2,152 @@ import numpy as np
 import cv2
 import torch
 import superpoint_pytorch
-
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-print(torch.__version__)
 
-detection_thresh = 0.005
-nms_radius = 5
+class Feature_detection():
+    def __init__(self, image_size):
+        self.input_image = None
+        self.input = None
+        self.model = superpoint_pytorch.SuperPoint_short(detection_threshold=0.005, nms_radius=5).eval()
+        self.model.load_state_dict(torch.load("weights/superpoint_v6_from_tf.pth"))
+        self.image_size = image_size
 
+    def get_input(self, image) -> None:
+        """
+        Preprocess raw image from camera or dataset.
+        """
+        img = cv2.resize(image, self.img_size)
+        self.input_image = img.copy()
+        self.input = img.mean(-1) / 255
 
-def match_descriptors(kp1, desc1, kp2, desc2):
-    # Match the keypoints with the warped_keypoints with nearest neighbor search
-    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+    def process_Superpoint(self) -> None:
+        """
+        Process NN and short post processing.
+        """
+        input_tensor = torch.from_numpy(self.input[None, None]).float()
+        scores, descriptors_dense = self.model(input_tensor)
+        pred_th_1 = self.post_processing_short(self, scores, descriptors_dense)
+        descriptors = pred_th_1['descriptors'][0].cpu().detach().numpy().astype(np.float32)
+        points_th = pred_th_1['keypoints'][0]
+        keypoints_np = np.array(points_th)  # Konwersja do NumPy
+        keypoints = [cv2.KeyPoint(float(p[0]), float(p[1]), 1) for p in keypoints_np]
+        return keypoints, descriptors
 
-    matches = bf.match(desc1, desc2)
-    matches_idx = np.array([m.queryIdx for m in matches])
-    m_kp1 = [kp1[idx] for idx in matches_idx]
-    matches_idx = np.array([m.trainIdx for m in matches])
-    m_kp2 = [kp2[idx] for idx in matches_idx]
+    def post_processing_short(self, scores, descriptors_dense):
+        """
+        Realise post processing including:
+        * discarding points near image border
+        * converting keypoints
+        * nms
+        """
+        conf = self.model.conf
+        b = scores.shape[0]
+        scores = superpoint_pytorch.batched_nms(scores, conf.nms_radius)
 
-    return m_kp1, m_kp2, matches
+        # Discard keypoints near the image borders
+        if conf.remove_borders:
+            pad = conf.remove_borders
+            scores[:, :pad] = -1
+            scores[:, :, :pad] = -1
+            scores[:, -pad:] = -1
+            scores[:, :, -pad:] = -1
 
-def compute_homography(matched_kp1, matched_kp2):
-    matched_pts1 = cv2.KeyPoint_convert(matched_kp1)
-    matched_pts2 = cv2.KeyPoint_convert(matched_kp2)
-
-    # Estimate the homography between the matches using RANSAC
-    H, inliers = cv2.findHomography(matched_pts1,
-                                    matched_pts2,
-                                    cv2.RANSAC)
-    inliers = inliers.flatten()
-    return H, inliers
-
-def preprocess_image(img_file, img_size):
-    img = cv2.imread(img_file, cv2.IMREAD_COLOR)
-    img = cv2.resize(img, img_size)
-    img_orig = img.copy()
-    image = img.mean(-1) / 255
-    img_preprocessed = np.pad(image, [(0, int(np.ceil(s/8))*8 - s) for s in image.shape[:2]])
-
-    return img_preprocessed, img_orig
-
-def post_processing_short(scores, descriptors_dense, conf):
-    b = scores.shape[0]
-    scores = superpoint_pytorch.batched_nms(scores, conf.nms_radius)
-
-    # Discard keypoints near the image borders
-    if conf.remove_borders:
-        pad = conf.remove_borders
-        scores[:, :pad] = -1
-        scores[:, :, :pad] = -1
-        scores[:, -pad:] = -1
-        scores[:, :, -pad:] = -1
-
-    # Extract keypoints
-    if b > 1:
-        idxs = torch.where(scores > conf.detection_threshold)
-        mask = idxs[0] == torch.arange(b, device=scores.device)[:, None]
-    else:  # Faster shortcut
-        scores = scores.squeeze(0)
-        idxs = torch.where(scores > conf.detection_threshold)
-
-    # Convert (i, j) to (x, y)
-    keypoints_all = torch.stack(idxs[-2:], dim=-1).flip(1).float()
-    scores_all = scores[idxs]
-
-    keypoints = []
-    scores = []
-    descriptors = []
-    for i in range(b):
+        # Extract keypoints
         if b > 1:
-            k = keypoints_all[mask[i]]
-            s = scores_all[mask[i]]
-        else:
-            k = keypoints_all
-            s = scores_all
-        if conf.max_num_keypoints is not None:
-            k, s = superpoint_pytorch.select_top_k_keypoints(k, s, conf.max_num_keypoints)
-        d = superpoint_pytorch.sample_descriptors(k[None], descriptors_dense[i, None], 2 ** (len(conf.channels) - 2))
-        keypoints.append(k)
-        scores.append(s)
-        descriptors.append(d.squeeze(0).transpose(0, 1))
+            idxs = torch.where(scores > conf.detection_threshold)
+            mask = idxs[0] == torch.arange(b, device=scores.device)[:, None]
+        else:  # Faster shortcut
+            scores = scores.squeeze(0)
+            idxs = torch.where(scores > conf.detection_threshold)
 
-    return {
-        "keypoints": keypoints,
-        "keypoint_scores": scores,
-        "descriptors": descriptors,
-    }
+        # Convert (i, j) to (x, y)
+        keypoints_all = torch.stack(idxs[-2:], dim=-1).flip(1).float()
+        scores_all = scores[idxs]
+
+        keypoints = []
+        scores = []
+        descriptors = []
+        for i in range(b):
+            if b > 1:
+                k = keypoints_all[mask[i]]
+                s = scores_all[mask[i]]
+            else:
+                k = keypoints_all
+                s = scores_all
+            if conf.max_num_keypoints is not None:
+                k, s = superpoint_pytorch.select_top_k_keypoints(k, s, conf.max_num_keypoints)
+            d = superpoint_pytorch.sample_descriptors(k[None], descriptors_dense[i, None], 2 ** (len(conf.channels) - 2))
+            keypoints.append(k)
+            scores.append(s)
+            descriptors.append(d.squeeze(0).transpose(0, 1))
+
+        return {
+            "keypoints": keypoints,
+            "keypoint_scores": scores,
+            "descriptors": descriptors,
+        }
+
+class VisualOdometry():
+    def __init__(self, image_size, start_R, start_t, K):
+        self.keypoints = {"past": None,
+                          "present": None}
+        self.matched_keypoints = {"past": None,
+                          "present": None}
+        self.descriptors = {"past": None,
+                          "present": None}
+        self.matches = None
+        self.feature_detection = Feature_detection(image_size)
+        self.total_R = start_R
+        self.total_t = start_t
+        self.K = K
+        self. trajectory = [start_t]
+
+    def match_descriptors(self):
+        """ Match the keypoints with the warped_keypoints with nearest neighbor search
+        """
+        bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+        kp1 = self.keypoints["past"]
+        kp2 = self.keypoints["present"]
+        self.matches = bf.match(self.descriptors["past"], self.descriptors["present"])
+        matches_idx = np.array([m.queryIdx for m in self.matches])
+        m_kp1 = [kp1[idx] for idx in matches_idx]
+        matches_idx = np.array([m.trainIdx for m in self.matches])
+        m_kp2 = [kp2[idx] for idx in matches_idx]
+
+        return m_kp1, m_kp2
+
+    def compute_homography(matched_kp1, matched_kp2):
+        matched_pts1 = cv2.KeyPoint_convert(matched_kp1)
+        matched_pts2 = cv2.KeyPoint_convert(matched_kp2)
+
+        # Estimate the homography between the matches using RANSAC
+        H, inliers = cv2.findHomography(matched_pts1,
+                                        matched_pts2,
+                                        cv2.RANSAC)
+        inliers = inliers.flatten()
+        return H, inliers
+    
+    def show_arrows(self):
+        for match in self.matches:
+            pt1 = tuple(map(int, self.keypoints["past"][match.queryIdx].pt))
+            pt2 = tuple(map(int, self.keypoints["present"][match.trainIdx].pt))
+            cv2.arrowedLine(self.feature_detection.input_image, pt2, pt1, (0, 255, 0), 1, tipLength=0.2)
+
+    def pose_estimation(self):
+        E, mask = cv2.findEssentialMat(self.matched_keypoints["past"], self.matched_keypoints["present"], self.K,  method=cv2.RANSAC, prob=0.999, threshold=3.0)
+        points, R, t, mask = cv2.recoverPose(E, self.matched_keypoints["past"], self.matched_keypoints["present"], self.K)
+        self.R_total = self.R_total @ R
+        self.t_total = self.t_total + self.R_total @ t
+
+    def compute_pipeline(self, image):
+        self.feature_detection.get_input(image)
+        self.keypoints["present"], self.descriptors["present"] = self.feature_detection.process_Superpoint()
+        m_kp1, m_kp2 = self.match_descriptors()
+        H, inliers = self.compute_homography(m_kp1, m_kp2)
+        self.matches = np.array(self.matches)[inliers.astype(bool)].tolist()
+        for match in self.matches:
+            self.matched_keypoints["past"].append(self.keypoints["past"][match.queryIdx].pt)
+            self.matched_keypoints["present"].append(self.keypoints["present"][match.trainIdx].pt)
+        self.pose_estimation()
+        self.trajectory.append(self.total_t.flatten().tolist())
